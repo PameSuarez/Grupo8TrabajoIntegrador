21f2849d16e7417f65fc1f5c5a6c011066743d55
MCAllegro <marcoscastroallegro@gmail.com>
2023-04-16 12:08:45 -0300

    Correcciones en wizard

--- script/pruebaWizard.js
+++ script/pruebaWizard.js
@@ -2,59 +2,45 @@ function success() {
   document.getElementsByClassName(".exito").style = "display:block";
 }
 
-jQuery(document).ready(function () {
-  // click on next button
-  jQuery(".form-wizard-next-btn").click(function () {
-    var parentFieldset = jQuery(this).parents(".wizard-fieldset");
-    var currentActiveStep = jQuery(this)
-      .parents(".form-wizard")
-      .find(".form-wizard-steps .active");
-    var next = jQuery(this);
-    var nextWizardStep = true;
-    parentFieldset.find(".wizard-required").each(function () {
-      var thisValue = jQuery(this).val();
+jQuery(document).ready(function() {
+	// click on next button
+	jQuery('.form-wizard-next-btn').click(function() {
+		var parentFieldset = jQuery(this).parents('.wizard-fieldset');
+		var currentActiveStep = jQuery(this).parents('.form-wizard').find('.form-wizard-steps .active');
+		var next = jQuery(this);
+		var nextWizardStep = true;
+		parentFieldset.find('.wizard-required').each(function(){
+			var thisValue = jQuery(this).val();
 
-      if (thisValue == "") {
-        jQuery(this).siblings(".wizard-form-error").slideDown();
-        nextWizardStep = false;
-      } else {
-        jQuery(this).siblings(".wizard-form-error").slideUp();
-      }
-    });
-    if (nextWizardStep) {
-      next.parents(".wizard-fieldset").removeClass("show", "400");
-      currentActiveStep
-        .removeClass("active")
-        .addClass("activated")
-        .next()
-        .addClass("active", "400");
-      next
-        .parents(".wizard-fieldset")
-        .next(".wizard-fieldset")
-        .addClass("show", "400");
-      jQuery(document)
-        .find(".wizard-fieldset")
-        .each(function () {
-          if (jQuery(this).hasClass("show")) {
-            var formAtrr = jQuery(this).attr("data-tab-content");
-            jQuery(document)
-              .find(".form-wizard-steps .form-wizard-step-item")
-              .each(function () {
-                if (jQuery(this).attr("data-attr") == formAtrr) {
-                  jQuery(this).addClass("active");
-                  var innerWidth = jQuery(this).innerWidth();
-                  var position = jQuery(this).position();
-                  jQuery(document)
-                    .find(".form-wizard-step-move")
-                    .css({ left: position.left, width: innerWidth });
-                } else {
-                  jQuery(this).removeClass("active");
-                }
-              });
-          }
-        });
-    }
-  });
+			if( thisValue == "") {
+				jQuery(this).siblings(".wizard-form-error").slideDown();
+				nextWizardStep = false;
+			}
+			else {
+				jQuery(this).siblings(".wizard-form-error").slideUp();
+			}
+		});
+		if( nextWizardStep) {
+			next.parents('.wizard-fieldset').removeClass("show","400");
+			currentActiveStep.removeClass('active').addClass('activated').next().addClass('active',"400");
+			next.parents('.wizard-fieldset').next('.wizard-fieldset').addClass("show","400");
+			jQuery(document).find('.wizard-fieldset').each(function(){
+				if(jQuery(this).hasClass('show')){
+					var formAtrr = jQuery(this).attr('data-tab-content');
+					jQuery(document).find('.form-wizard-steps .form-wizard-step-item').each(function(){
+						if(jQuery(this).attr('data-attr') == formAtrr){
+							jQuery(this).addClass('active');
+							var innerWidth = jQuery(this).innerWidth();
+							var position = jQuery(this).position();
+							jQuery(document).find('.form-wizard-step-move').css({"left": position.left, "width": innerWidth});
+						}else{
+							jQuery(this).removeClass('active');
+						}
+					});
+				}
+			});
+		}
+	});
   //click on previous button
   jQuery(".form-wizard-previous-btn").click(function () {
     var counter = parseInt(jQuery(".wizard-counter").text());
