f7478611ae1d49ff8f37bec00d0f7a336ba104b7
MCAllegro <marcoscastroallegro@gmail.com>
2023-04-17 10:47:10 -0300

    Validación de email funcionando

--- script/pruebaWizard.js
+++ script/pruebaWizard.js
@@ -2,55 +2,71 @@ function success() {
   document.getElementsByClassName(".exito").style = "display:block";
 }
 
-jQuery(document).ready(function() {
-  
-	// click on next button
-	jQuery('.form-wizard-next-btn').click(function() {
-    $("#basic-form").validate();
-		var parentFieldset = jQuery(this).parents('.wizard-fieldset');
-		var currentActiveStep = jQuery(this).parents('.form-wizard').find('.form-wizard-steps .active');
-		var next = jQuery(this);
-		var nextWizardStep = true;
-		parentFieldset.find('.wizard-required').each(function(){
-			var thisValue = jQuery(this).val();
-      var email = $(".input input[name=email]").val();
+jQuery(document).ready(function () {
+  // click on next button
+  jQuery(".form-wizard-next-btn").click(function () {
+    var parentFieldset = jQuery(this).parents(".wizard-fieldset");
+    var currentActiveStep = jQuery(this)
+      .parents(".form-wizard")
+      .find(".form-wizard-steps .active");
+    var next = jQuery(this);
+    var nextWizardStep = true;
+    parentFieldset.find(".wizard-required").each(function () {
+      var thisValue = jQuery(this).val();
+      var email = $("#email").val();
       var regex = /^([a-zA-Z0-9_\.\+\-])+\@(([a-zA-z0-9\-])+\.)+([a-zA-Z0-9]{2,4})$/;
 
-			if( thisValue == "") {
-        jQuery(this).siblings(".wizard-form-error .wizard-email.error").slideDown();
+      if (thisValue == "") {
+        jQuery(this).siblings(".wizard-form-error").slideDown();
+       // jQuery(this).find(".wizard-email-error").slideDown();
         console.log("valores");
-				nextWizardStep = false;
-			}
-
-       
+        nextWizardStep = false;
+      }
+        if(!regex.test(email)){
+          jQuery(this).siblings(".wizard-email-error").slideDown();
+          console.log("mail");
+          nextWizardStep = false;
+        }
+       else {
+        jQuery(this).siblings(".wizard-form-error").slideUp();
+        jQuery(this).siblings(".wizard-email-error").slideUp();
+      }
     
-     
-			else {
-				jQuery(this).siblings(".wizard-form-error .wizard-email.error").slideUp();
-        
-    	}
-		});
-		if( nextWizardStep) {
-			next.parents('.wizard-fieldset').removeClass("show","400");
-			currentActiveStep.removeClass('active').addClass('activated').next().addClass('active',"400");
-			next.parents('.wizard-fieldset').next('.wizard-fieldset').addClass("show","400");
-			jQuery(document).find('.wizard-fieldset').each(function(){
-				if(jQuery(this).hasClass('show')){
-					var formAtrr = jQuery(this).attr('data-tab-content');
-					jQuery(document).find('.form-wizard-steps .form-wizard-step-item').each(function(){
-						if(jQuery(this).attr('data-attr') == formAtrr){
-							jQuery(this).addClass('active');
-							var innerWidth = jQuery(this).innerWidth();
-							var position = jQuery(this).position();
-							jQuery(document).find('.form-wizard-step-move').css({"left": position.left, "width": innerWidth});
-						}else{
-							jQuery(this).removeClass('active');
-						}
-					});
-				}
-			});
-		}
-	});
+    });
+    if (nextWizardStep) {
+      next.parents(".wizard-fieldset").removeClass("show", "400");
+      currentActiveStep
+        .removeClass("active")
+        .addClass("activated")
+        .next()
+        .addClass("active", "400");
+      next
+        .parents(".wizard-fieldset")
+        .next(".wizard-fieldset")
+        .addClass("show", "400");
+      jQuery(document)
+        .find(".wizard-fieldset")
+        .each(function () {
+          if (jQuery(this).hasClass("show")) {
+            var formAtrr = jQuery(this).attr("data-tab-content");
+            jQuery(document)
+              .find(".form-wizard-steps .form-wizard-step-item")
+              .each(function () {
+                if (jQuery(this).attr("data-attr") == formAtrr) {
+                  jQuery(this).addClass("active");
+                  var innerWidth = jQuery(this).innerWidth();
+                  var position = jQuery(this).position();
+                  jQuery(document)
+                    .find(".form-wizard-step-move")
+                    .css({ left: position.left, width: innerWidth });
+                } else {
+                  jQuery(this).removeClass("active");
+                }
+              });
+          }
+        });
+    }
+  });
   //click on previous button
   jQuery(".form-wizard-previous-btn").click(function () {
     var counter = parseInt(jQuery(".wizard-counter").text());
@@ -151,16 +167,21 @@ jQuery(document).ready(function() {
 
     var negocio = document.getElementById("typeBusiness").value;
 
-	document.getElementById("listarNombre").innerHTML = "Nombre: " + nombre;
-	document.getElementById("listarApellido").innerHTML = "Apellido: " + apellido;
-	document.getElementById("listarEmail").innerHTML = "Email: " + email;
-	document.getElementById("listarPais").innerHTML = "País: " + pais;
-	document.getElementById("listarCiudad").innerHTML = "Ciudad: " + ciudad;
-	document.getElementById("listarDomicilio").innerHTML = "Domicilio: " + domicilio;
-	document.getElementById("listarProducto").innerHTML = "Producto: " + valorSeleccionado1;
-	document.getElementById("listarActividad").innerHTML = "Actividad: " + valorSeleccionado2;
-	document.getElementById("listarNegocio").innerHTML = "Tipo de negocio: " + negocio;
+    document.getElementById("listarNombre").innerHTML = "Nombre: " + nombre;
+    document.getElementById("listarApellido").innerHTML =
+      "Apellido: " + apellido;
+    document.getElementById("listarEmail").innerHTML = "Email: " + email;
+    document.getElementById("listarPais").innerHTML = "País: " + pais;
+    document.getElementById("listarCiudad").innerHTML = "Ciudad: " + ciudad;
+    document.getElementById("listarDomicilio").innerHTML =
+      "Domicilio: " + domicilio;
+    document.getElementById("listarProducto").innerHTML =
+      "Producto: " + valorSeleccionado1;
+    document.getElementById("listarActividad").innerHTML =
+      "Actividad: " + valorSeleccionado2;
+    document.getElementById("listarNegocio").innerHTML =
+      "Tipo de negocio: " + negocio;
   });
-
+  $("#basic-form").validate();
 });
 
